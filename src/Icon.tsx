ef84d83c4f6a5dcf8c7249371423a367baeb69f4
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-05 17:46:35 -0700

    .

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -2,17 +2,11 @@ import React, { FC } from 'react';
 import styles from './Icon.module.scss';
 import { IconProps } from './Icon.prop';
 import { images } from './SvgPath';
-const sizes = {
-  small: { width: '14px', height: '14px' },
-  medium: { width: '18px', height: '18px' },
-  large: { width: '24px', height: '24px' },
-  xlarge: { width: '32px', height: '32px' },
-};
 const Icon: FC<IconProps> = ({ icon, color, size = 'medium' }) => {
   const SvgIcon = images[icon];
-  const iconClass = `${styles.icon} ${color ? styles[`icon--accent`] : ''}`.trim();
+  const iconClass = `${styles.icon} ${styles[`icon--${size}`]} ${color ? styles[`icon--accent`] : ''}`.trim();
 
-  return <SvgIcon data-accent-color={color} className={iconClass} style={sizes[size]} />;
+  return <SvgIcon data-accent-color={color} className={iconClass} />;
 };
 
 export default Icon;
