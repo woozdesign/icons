5769993110b1dbeac9598e9ebd859af34da13917
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-10-05 18:25:05 -0700

    fixed color

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -5,13 +5,16 @@ import { images } from './SvgPath';
 const Icon: FC<IconProps> = ({ type, color, size = 'medium', className, style }) => {
   const SvgIcon = images[type];
   // const wrapperClass = `${styles[`wrapper--${size}`]} `;
-  const iconClass = `${styles.icon} ${styles[`icon--${size}`]} ${(color ? styles[`icon--accent`] : '', className)}`.trim();
+  const iconClasses = [styles.icon, styles[`icon--${size}`], color ? styles[`icon--accent`] : '', className ?? ''];
 
   return (
     // <span className={wrapperClass}>
-    <SvgIcon data-accent-color={color} className={iconClass} style={style} />
+    <SvgIcon data-accent-color={color} className={combineClassNames(iconClasses)} style={style} />
     // </span>
   );
 };
 
 export default Icon;
+export const combineClassNames = (arr: Array<string>) => {
+  return arr.filter(Boolean).join(' ');
+};
